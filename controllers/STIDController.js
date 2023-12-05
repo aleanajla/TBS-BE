98b9fd18a41bea5d912aa709c4f6d7f50a0bf517
anidadewip <anidadewip@gmail.com>
2023-12-05 11:37:44 +0700

    Update: STID Controller

--- controllers/STIDController.js
+++ controllers/STIDController.js
@@ -6,9 +6,9 @@ const Driver = db.masterDriver
 const STID = db.masterSTID
 
 
-module.exports.viewTruck = async (req,res) => {
+module.exports.viewTruck = async (req, res) => {
     const ID_Customer = req.params.id
-    try{
+    try {
         const dataTruck = await Truck.findAll({
             where: {
                 ID_Customer: ID_Customer
@@ -17,14 +17,14 @@ module.exports.viewTruck = async (req,res) => {
 
         res.status(200).send(dataTruck)
     }
-    catch(error){
-        res.status(500).send({message : error.message})
+    catch (error) {
+        res.status(500).send({ message: error.message })
     }
 }
 
-module.exports.viewDriver = async (req,res) => {
+module.exports.viewDriver = async (req, res) => {
     const ID_Customer = req.params.id
-    try{
+    try {
         const dataDriver = await Driver.findAll({
             where: {
                 ID_Customer: ID_Customer
@@ -33,24 +33,24 @@ module.exports.viewDriver = async (req,res) => {
 
         res.status(200).send(dataDriver)
     }
-    catch(error) {
-        res.status(500).send({message : error.message})
+    catch (error) {
+        res.status(500).send({ message: error.message })
     }
 }
 
-module.exports.addSTID = async(req,res) => {
-    const {Driver_ID, Truck_ID} = req.body
+module.exports.addSTID = async (req, res) => {
+    const { Driver_ID, Truck_ID } = req.body
     let flag = 0
     let STIDNumber = null;
-    
-    try{
+
+    try {
         // check driver & truck only can be 1
         const check_driver = await STID.findOne({
             where: {
                 Driver_ID: Driver_ID
             }
         })
-        if(check_driver){
+        if (check_driver) {
             return res.status(404).send("Driver Already Assign!");
         }
         console.log(check_driver);
@@ -60,27 +60,27 @@ module.exports.addSTID = async(req,res) => {
                 Truck_ID: Truck_ID
             }
         })
-        if(check_truck){
+        if (check_truck) {
             return res.status(404).send("Truck Already Assign!");
         }
         console.log(check_truck);
 
         // generate STID_Number
-        do{
+        do {
             const randomNumbers = Math.floor(100000 + Math.random() * 900000);
             STIDNumber = `C${randomNumbers}`;
-        
+
             const searchSTID = await STID.findOne({
                 where: {
                     STID_Number: STIDNumber
                 }
             })
 
-            if(!searchSTID){
+            if (!searchSTID) {
                 flag = 1;
                 break;
             }
-        }while(flag == 1);
+        } while (flag == 1);
 
         const createSTID = await STID.create({
             STID_Number: STIDNumber,
@@ -90,18 +90,18 @@ module.exports.addSTID = async(req,res) => {
 
         res.status(200).send(createSTID)
     }
-    catch(error){
-        res.status(500).send({message : error.message})
+    catch (error) {
+        res.status(500).send({ message: error.message })
     }
 }
 
 module.exports.searchSTID = async (req, res) => {
-    const {stid, ID_Customer} = req.query;
-    try{
+    const { stid, ID_Customer } = req.query;
+    try {
         const search = await STID.findAll({
             where: {
                 STID_Number: db.sequelize.where(db.sequelize.fn('lower', db.sequelize.col("STID_Number")), {
-                    [Op.like] : `%${stid}%`
+                    [Op.like]: `%${stid}%`
                 })
             },
             include: [
@@ -116,57 +116,61 @@ module.exports.searchSTID = async (req, res) => {
         console.log(search);
         res.status(200).send(search);
     }
-    catch (error){
+    catch (error) {
         console.log(error);
-        res.status(500).send({message : error.message})
+        res.status(500).send({ message: error.message })
     }
 }
 
 module.exports.searchTruck = async (req, res) => {
-    const {truck, ID_Customer} = req.query;
-    try{
+    const { truck, ID_Customer } = req.query;
+    try {
         const search = await Truck.findAll({
             where: {
                 [Op.and]: [
-                    {Plat_Number: db.sequelize.where(db.sequelize.fn('lower', db.sequelize.col("Plat_Number")), {
-                        [Op.like]: `%${truck}%`
-                    })},
-                    {ID_Customer: ID_Customer}
+                    {
+                        Plat_Number: db.sequelize.where(db.sequelize.fn('lower', db.sequelize.col("Plat_Number")), {
+                            [Op.like]: `%${truck}%`
+                        })
+                    },
+                    { ID_Customer: ID_Customer }
                 ]
             }
         })
         res.status(200).send(search);
     }
-    catch(error){
-        res.status(500).send({message : error.message})
+    catch (error) {
+        res.status(500).send({ message: error.message })
     }
 }
 
 module.exports.searchDriver = async (req, res) => {
-    const {driver, ID_Customer} = req.query;
-    try{
+    const { driver, ID_Customer } = req.query;
+    try {
         const search = await Driver.findAll({
             where: {
                 [Op.and]: [
-                    {Driver_Name: db.sequelize.where(db.sequelize.fn('lower', db.sequelize.col("Driver_Name")), {
-                        [Op.like]: `%${driver}%`
-                    })},
-                    {ID_Customer: ID_Customer}
+                    {
+                        Driver_Name: db.sequelize.where(db.sequelize.fn('lower', db.sequelize.col("Driver_Name")), {
+                            [Op.like]: `%${driver}%`
+                        })
+                    },
+                    { ID_Customer: ID_Customer }
                 ]
             }
         })
         res.status(200).send(search);
     }
-    catch (error){
-        res.status(500).send({message : error.message})
+    catch (error) {
+        res.status(500).send({ message: error.message })
     }
 }
 
-module.exports.viewSTID = async (req,res) => {
+module.exports.viewSTID = async (req, res) => {
     const ID_Customer = req.params.id
-    try{
+    try {
         const result = await STID.findAll({
-            attributes: ['id','STID_Number'],
+            attributes: ['id', 'STID_Number'],
             include: [
                 {
                     model: Driver,
@@ -186,53 +190,67 @@ module.exports.viewSTID = async (req,res) => {
         })
         res.status(200).send(result)
     }
-    catch(error) {
-        res.status(500).send({message : error.message})
+    catch (error) {
+        res.status(500).send({ message: error.message })
     }
 }
 
-module.exports.editSTID = async (req,res) => {
+module.exports.editSTID = async (req, res) => {
     console.log(req.body);
-    const {Driver_ID, Truck_ID, id} = req.body
-    try{
-        const update = await STID.update({
-            Driver_ID,
-            Truck_ID,
-            updatedAt: new Date()
-        }, {
+    const { Driver_ID, id } = req.body
+    try {
+        const checkDriver = await STID.findOne({
             where: {
-                id: id
+                Driver_ID
             }
-        })
+        });
+
+        if (checkDriver) {
+            return res.status(400).send("Driver already assigned!");
+        }
+
+        await STID.update(
+            {
+                Driver_ID
+            },
+            {
+                where: {
+                    id
+                }
+            }
+        );
 
         const updatedData = await STID.findOne({
-            where:{
+            where: {
                 id
             }
-        })
+        });
 
-        res.status(200).json(updatedData)
+        if (!updatedData) {
+            return res.status(404).send("Record not found");
+        }
+
+        res.status(200).send(updatedData);
     }
-    catch(error) {
-        res.status(500).send({message : error.message})
+    catch (error) {
+        res.status(500).send({ message: error.message })
     }
 }
 
 
-module.exports.deleteSTID = async (req,res) => {
-    // tinggal nambahin id customernya
-    const {id} = req.body
-    try{
+module.exports.deleteSTID = async (req, res) => {
+    const { id } = req.body
+    try {
         const remove = await STID.destroy({
-            where : {
-                id: id
+            where: {
+                id
             }
         })
         console.log(remove);
         res.status(200).send("Successfully Deleted")
     }
-    catch(error){
-        res.status(500).send({message : error.message})
+    catch (error) {
+        res.status(500).send({ message: error.message })
     }
 }
 
